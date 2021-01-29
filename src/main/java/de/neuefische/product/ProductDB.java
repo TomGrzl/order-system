3c37d6b06a6bc7479a81d53d8809e8c57efba994
Jothaka <jhkahle@hotmail.de>
2021-01-29 12:58:08 +0100

    Added getProductListTest, getProductTest & implementation of tested methods

--- src/main/java/de/neuefische/product/ProductDB.java
+++ src/main/java/de/neuefische/product/ProductDB.java
@@ -1,9 +1,6 @@
 package de.neuefische.product;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class ProductDB {
 
@@ -11,23 +8,16 @@ public class ProductDB {
 
     public ProductDB(Product[] products){
         for (Product product : products) {
-
             productList.put(product.getId() ,product);
-
         }
-
     }
 
     public List<Product> getProductList() {
-
-
-
-        return Arrays.asList(productList.values().toArray());
+        List<Product> result = new ArrayList<>(productList.values());
+        return result;
     }
 
     public Product getProduct(Integer productId) {
-
-        return null;
+        return productList.get(productId);
     }
-
 }
--- src/test/java/de/neuefische/product/ProductDBTest.java
+++ src/test/java/de/neuefische/product/ProductDBTest.java
@@ -1,35 +1,61 @@
 package de.neuefische.product;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductDBTest {
 
-
     @Test
-    public void getProductListTest(){
+    public void getProductListTest() {
         // GIVEN
         Product[] products = new Product[]{
-                new Product("Kaese"),
-                new Product("Wurst")
+                new Product("Cheese"),
+                new Product("Sausage")
         };
         ProductDB productDB = new ProductDB(products);
 
-        //WHEN
+        // WHEN
         Collection<Product> actual = productDB.getProductList();
 
-        //THEN
+        // THEN
         Collection<Product> expected = Arrays.asList(products);
         assertEquals(expected, actual);
     }
 
+    @ParameterizedTest
+    @MethodSource
+    public void getProductTest(ProductDB productDB, Integer id, Product expected) {
 
+        // WHEN
+        Product actual = productDB.getProduct(id);
 
+        // THEN
+        assertEquals(expected, actual);
+    }
 
+    private static Stream<Arguments> getProductTest() {
+        // GIVEN
+        Product cheese = new Product("Cheese");
+        Product sausage = new Product("Sausage");
+        Product bread = new Product("Bread");
 
+        Product[] products = new Product[]{
+                cheese, sausage
+        };
+        ProductDB productDB = new ProductDB(products);
 
+        return  Stream.of(
+          Arguments.arguments(productDB,cheese.getId(), cheese),
+          Arguments.arguments(productDB,sausage.getId(),sausage),
+          Arguments.arguments(productDB, bread.getId(), null)
+        );
+    }
 }
