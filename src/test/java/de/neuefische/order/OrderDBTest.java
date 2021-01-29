76d421b6b2c217b641d36dfd158ec50684b14aef
Tom Grehl <grehl.tom@googlemail.com>
2021-01-29 11:21:36 +0100

    Added test classes

---
+++ src/test/java/de/neuefische/order/OrderDBTest.java
@@ -0,0 +1,7 @@
+package de.neuefische.order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderDBTest {
+
+}
---
+++ src/test/java/de/neuefische/product/ProductDBTest.java
@@ -0,0 +1,7 @@
+package de.neuefische.product;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDBTest {
+
+}
