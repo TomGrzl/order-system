73339915df36da16acdc31c2b5227f3be4ec21f4
Felix <Felix@hh.de>
2021-01-29 12:14:33 +0100

    Add First Test

--- src/main/java/de/neuefische/product/ProductDB.java
+++ src/main/java/de/neuefische/product/ProductDB.java
@@ -1,14 +1,28 @@
 package de.neuefische.product;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 public class ProductDB {
-    private HashMap<Integer, Product> productList;
 
-    public Collection<Product> getProductList() {
+    private HashMap<Integer, Product> productList = new HashMap<>();
 
-        return null;
+    public ProductDB(Product[] products){
+        for (Product product : products) {
+
+            productList.put(product.getId() ,product);
+
+        }
+
+    }
+
+    public List<Product> getProductList() {
+
+
+
+        return Arrays.asList(productList.values().toArray());
     }
 
     public Product getProduct(Integer productId) {
--- src/test/java/de/neuefische/product/ProductDBTest.java
+++ src/test/java/de/neuefische/product/ProductDBTest.java
@@ -1,7 +1,35 @@
 package de.neuefische.product;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductDBTest {
+
+
+    @Test
+    public void getProductListTest(){
+        // GIVEN
+        Product[] products = new Product[]{
+                new Product("Kaese"),
+                new Product("Wurst")
+        };
+        ProductDB productDB = new ProductDB(products);
+
+        //WHEN
+        Collection<Product> actual = productDB.getProductList();
+
+        //THEN
+        Collection<Product> expected = Arrays.asList(products);
+        assertEquals(expected, actual);
+    }
+
+
+
+
+
 
 }
